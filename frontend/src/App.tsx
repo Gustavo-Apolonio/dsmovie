3b04fb510c57c22d0e3764e8b58bda513365e032
Gustavo Apolonio <gustta.apolonio@gmail.com>
2022-02-01 00:57:03 -0300

    Project's Front -- Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
+import Navbar from "./components/navbar";
+
 function App() {
-  return <h1>Home page</h1>;
+  return <Navbar />;
 }
 
 export default App;
---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,27 @@
+import { ReactComponent as GithubIcon } from "assets/images/github.svg";
+
+import "./styles.css"
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a
+            href="https://github.com/Gustavo-Apolonio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/Gustavo-Apolonio</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
