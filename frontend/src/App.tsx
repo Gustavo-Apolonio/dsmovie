d1d1d25bce10544bc8de5718cdfe12ddef787e79
Gustavo Apolonio <gustta.apolonio@gmail.com>
2022-02-01 01:02:40 -0300

    Project's Front -- Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
-import Navbar from "./components/navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "pages/listing";
+import Form from "pages/form";
+import Navbar from "components/navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/listing/index.tsx
@@ -0,0 +1,5 @@
+function Listing() {
+  return <h1>Listing</h1>;
+}
+
+export default Listing;
